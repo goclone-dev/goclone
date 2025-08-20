08b7eab8effb23ae0158ef9cd09a79067f710f5a
Juan Mesaglio <juan.mesaglio@gmail.com>
2025-08-19 22:47:20 -0300

    feat(errors): less panic, return errors
    Signed-off-by: Juan Mesaglio <juan.mesaglio@gmail.com>

--- pkg/crawler/extractor.go
+++ pkg/crawler/extractor.go
@@ -26,13 +26,13 @@ var (
 // Extractor visits a link determines if its a page or sublink
 // downloads the contents to a correct directory in project folder
 // TODO add functionality for determining if page or sublink
-func Extractor(link string, projectPath string) {
+func Extractor(link string, projectPath string) error {
 	fmt.Println("Extracting --> ", link)
 
 	// get the html body
 	resp, err := http.Get(link)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to GET %s: %w", link, err)
 	}
 
 	// Closure
@@ -50,32 +50,38 @@ func Extractor(link string, projectPath string) {
 		dirPath := extensionDir[ext]
 		if dirPath != "" {
 			// If extension and path are valid pass to writeFileToPath
-			writeFileToPath(projectPath, base, dirPath, resp)
+			if err := writeFileToPath(projectPath, base, dirPath, resp); err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
 
-func writeFileToPath(projectPath, filename, fileDir string, resp *http.Response) {
+func writeFileToPath(projectPath, filename, fileDir string, resp *http.Response) error {
 	// Create the full path
 	fullPath := filepath.Join(projectPath, fileDir, filename)
 
 	// Create the directory if it doesn't exist
 	err := os.MkdirAll(filepath.Dir(fullPath), 0777)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to create directories for %s: %w", fullPath, err)
 	}
 
 	// Open the file for writing
 	f, err := os.OpenFile(fullPath, os.O_RDWR|os.O_CREATE, 0777)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to open file %s: %w", fullPath, err)
 	}
 	defer f.Close()
 
 	// Read and write the file contents
 	htmlData, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to read response body: %w", err)
 	}
-	f.Write(htmlData)
+	if _, err := f.Write(htmlData); err != nil {
+		return fmt.Errorf("failed to write file %s: %w", fullPath, err)
+	}
+	return nil
 }
